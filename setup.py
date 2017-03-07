d2709278b1fe1d39087b751693eb0cc364c9a4a8
weitang114 <weitang114@gmail.com>
2017-03-08 01:55:52 +0800

    Upload to pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fmq',
+      version='0.1',
+      description='Fast MP Queue, Feed Me Queue',
+      url='https://github.com/weitang114/FMQ',
+      author='Tang Lee',
+      author_email='weitang114@gmail.com',
+      license='MIT',
+      packages=['fmq'],
+      zip_safe=False)
